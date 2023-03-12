c4ffebeb4f9b423c0558bc7954169a1567fe053b
skmdmahasin <mdmahasinineuron@gmail.com>
2023-03-12 11:58:40 +0530

    Data ingestion is completed

--- src/logger.py
+++ src/logger.py
@@ -13,6 +13,5 @@ logging.basicConfig(
     format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO)
 
-
-if __name__ == "__main__":
-    logging.info("logginf has started")
+if __name__=="__main__":
+    logging.info("logger test")
--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-from datetime import datetime
+class Customer:
 
-print(datetime.now())
+    def __init__(self,name,age,adress) -> None:
+        self.name = name
+        self.age = age
+        self.adress = adress
+
+class Address:
+
+    def __init__(self,city,pincode) -> None:
+        self.city = city
+        self.pincode = pincode
+
+Cust_1_adress = Address("Bangalore", 560057)
+
+cust1_profile = Customer("Aayan",23,Cust_1_adress)
+
+print(cust1_profile.adress.city)
