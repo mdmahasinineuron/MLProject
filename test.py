49efe8790c0735a84dfe0fa4260bc142e9de4937
skmdmahasin <mdmahasinineuron@gmail.com>
2023-03-16 12:46:54 +0530

    pickel file is created

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
-class Customer:
+from src.logger import logging
+from src.exception import CustomException
+import sys
 
-    def __init__(self,name,age,adress) -> None:
-        self.name = name
-        self.age = age
-        self.adress = adress
-
-class Address:
-
-    def __init__(self,city,pincode) -> None:
-        self.city = city
-        self.pincode = pincode
-
-Cust_1_adress = Address("Bangalore", 560057)
-
-cust1_profile = Customer("Aayan",23,Cust_1_adress)
-
-print(cust1_profile.adress.city)
+logging.info("test log")
